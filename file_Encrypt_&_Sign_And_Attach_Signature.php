4bd4e40b2d795f111c1ce3131fe59b984f107831
mohitesachin217 <7931096+mohitesachin217@users.noreply.github.com>
2024-06-21 15:36:54 +0530

    Update file_Encrypt_&_Sign_And_Attach_Signature.php

--- file_Encrypt_&_Sign_And_Attach_Signature.php
+++ file_Encrypt_&_Sign_And_Attach_Signature.php
@@ -2,11 +2,21 @@
 // Text to be encrypted
 $textToEncrypt = file_get_contents('bob.txt');
 
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 1:Text to be incrypted :</br> ".$textToEncrypt;
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
 // Load public key
-$publicKey = openssl_pkey_get_public(file_get_contents("public_key.pem"));
+$publicKey = openssl_pkey_get_public(file_get_contents("public.pem"));
 
 // Load private key
-$privateKey = openssl_pkey_get_private(file_get_contents("private_key.pem"));
+$privateKey = openssl_pkey_get_private(file_get_contents("private.pem"));
 
 
 // Encrypt the text using the public key
@@ -15,22 +25,73 @@ openssl_public_encrypt($textToEncrypt, $encryptedText, $publicKey, OPENSSL_PKCS1
 // Encoded encrypted text for safe storage or transmission
 $encodedEncryptedText = base64_encode($encryptedText);
 
-echo "Encrypted Text: $encodedEncryptedText\n";
+
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 2:Encrypted Text with public key :</br> $encodedEncryptedText\n";
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
 
 
 /**
  * _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+__
  */
+
 // sign the message
 $hash = hash('sha256',$textToEncrypt);
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 3: Hash created from text to encrypt with hash function hash('sha256',#textToEncrypt) for signature Text :
+    </br>
+    You can call it as a 'Signature':
+    </br>
+     $hash\n
+     </br>
+     and hex representation of hash :</br>
+     ".bin2hex($hash);
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+
 // create the signature
 $signature = '';
 if(!openssl_private_encrypt($hash,$signature,$privateKey)){
     die('failed to sign the message'.openssl_error_string());
 }
 
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 4: Encrypted signature [step 3:] created with private key :</br> $signature\n";
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+
 $signatureHex = bin2hex($signature);
-echo $signatureHex.'\n';
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 5:Hex value of the signature [step 3:] converted in hex with bin2hex():</br> $signatureHex\n";
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
 /**
  * _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+__
  */
@@ -48,6 +109,20 @@ file_put_contents($filePath, $encodedEncryptedText . $delimiter . $signatureHex)
  */
 
 
+
+$fileHdd =  file_get_contents($filePath);
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 6:File converted and saved with signature attached to it:</br> $fileHdd\n";
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+
+
 // stored file on hdd
 
 // Encoded encrypted text
@@ -59,13 +134,19 @@ $encryptedText = base64_decode($encodedEncryptedText);
 
 // Decrypt the encrypted text using the private key
 openssl_private_decrypt($encryptedText, $decryptedText, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-echo "<br/>";
-echo "Decrypted Text: $decryptedText\n";
-echo "<br/>";
-/**
- * _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+__
- */
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 7: Decrypted text with private key which is encrypted with public key in step 2 :</br> $decryptedText\n";
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
 $oHash = hash('sha256',$decryptedText);
+
 
 $oSignature = '';
 if(!openssl_public_decrypt($signature,$decrypted,$publicKey)){
@@ -73,13 +154,57 @@ if(!openssl_public_decrypt($signature,$decrypted,$publicKey)){
 }
 
 $verificationHex = bin2hex($decrypted);
-echo "<br/>";
-echo $verificationHex.'\n';
+
+
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 8 : Hash of the decrypted text you can call it 'Signature' on receiver side :</br> $oHash\n";
+echo "</br>";
+echo "and hex representation of hash :</br> $verificationHex\n";
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+ 
+/**
+ * _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+__
+ */
+
+
+
+
+
+
+
 $hashHex = bin2hex($hash);
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 9 : Hash signature in Step 3 created with hash function hash('sha256',#textToEncrypt) for  : $hashHex\n";
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+ 
+
 if($verificationHex == $hashHex){
-    echo "<br/>";
-    echo "signature is valid\n";
-    echo "<br/>";
+    echo "</br>";
+    echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+    echo "</br>";
+    echo "Step 10: check the signature that is not attached to the file step 8 and step 9 verificationHex == hashHex:</br>Signature is valid:\n";
+    echo "</br>";
+    echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+    echo "</br>";
+    echo "</br>";
+    echo "</br>";
+
 }else{
     echo "problem";
 }
@@ -96,6 +221,18 @@ if($verificationHex == $hashHex){
 
  // Read the file contents including the signature
 $fileContentsWithSignature = file_get_contents($filePath);
+
+
+    echo "</br>";
+    echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+    echo "</br>";
+    echo "Step 11: read or recived file from source (network or HDD):</br>\n".$fileContentsWithSignature;
+    echo "</br>";
+    echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+    echo "</br>";
+    echo "</br>";
+    echo "</br>";
+
 if ($fileContentsWithSignature === false) {
     die('Failed to read file: ' . $filePath);
 }
@@ -103,8 +240,19 @@ if ($fileContentsWithSignature === false) {
 $delimiter = "\n--SIGNATURE--\n";
 list($fileContents, $signatureHex) = explode($delimiter, $fileContentsWithSignature);
 
-// Convert the signature from hexadecimal to binary
-$signature = hex2bin($signatureHex);
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 12: Encrypted text from file :\n".$fileContents;
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+
+
 
 $encryptedText = $fileContents;
 
@@ -113,23 +261,87 @@ $encryptedText = base64_decode($encodedEncryptedText);
 
 // Decrypt the encrypted text using the private key
 openssl_private_decrypt($encryptedText, $decryptedText, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-echo "<br/>";
-echo "\nDecrypted Text: $decryptedText\n";
-echo "<br/>";
-$oHash = hash('sha256',$decryptedText);
+ 
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 12: Decrypted Text writter in file: :</br>\n".$decryptedText;
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
 
-$oSignature = '';
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 13: signature of the attched with file in hex :\n".$signatureHex;
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+
+// Convert the signature from hexadecimal to binary
+$signature = hex2bin($signatureHex);
+
+
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 14: signature converted back to bin with hex2bin() :</br>\n".$signature;
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
+
+
+$oHash = hash('sha256',$decryptedText);
+ 
 if(!openssl_public_decrypt($signature, $decrypted, $publicKey)){
-    die('failed to verify the signature'.openssl_error_string());
+    echo 'failed to verify the signature'.openssl_error_string();
 }
 
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 14: signature decrypted :</br>\n".$decrypted;
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+ 
 $verificationHex = bin2hex($decrypted);
 echo "<br/>";
-echo $verificationHex.'\n';
+
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "Step 15: Hex representation of the decrypted signature :</br>\n".$verificationHex;
+echo "</br>";
+echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+
 $hashHex = bin2hex($hash);
 if($verificationHex == $hashHex){
+    echo "</br>";
+    echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+    echo "</br>";
+    echo "Step 16: Verify the signature to check if tampering is done or not verificationHex == hashHex :</br>signature is valid for stored file";
+    echo "</br>";
+    echo "_+_+_+_++_+_+_+_+_+_+_+_+_+_+_+_++_+_\n";
+    echo "</br>";
+    echo "</br>";
+    echo "</br>";
     echo "<br/>";
-    echo "signature is valid for stored file\n";
     echo "<br/>";
 }else{
     echo "problem";
